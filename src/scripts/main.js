300a80b212be36d2e3a36c64115d6af2376de066
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-11-25 23:18:38 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const item in data) {
+    const li = document.createElement('li');
+
+    li.textContent = item;
+
+    if (Object.values(data[item]).length > 0) {
+      element.append(li);
+
+      const ul = document.createElement('ul');
+
+      li.append(ul);
+      createTree(ul, data[item]);
+    }
+    element.append(li);
+  }
 }
 
 createTree(tree, food);
